400c599e98de918c8cff5455be54ebae7e4e9722
Omar Maher <omarmaher100@gmail.com>
2020-02-03 15:55:36 +0300

    Create services and register it in app.module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DeleteBookComponent } from './components/delete-book/delete-book.compon
 import { NewBookComponent } from './components/new-book/new-book.component';
 import { ShowBookComponent } from './components/show-book/show-book.component';
 import { UpdateBookComponent } from './components/update-book/update-book.component';
+import { BookService } from './services/book.service';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,7 @@ import { UpdateBookComponent } from './components/update-book/update-book.compon
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
