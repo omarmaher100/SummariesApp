a777a08819dbe0d7b01841dd771ed9055ebdfb97
Omar Maher <omarmaher100@gmail.com>
2020-02-04 10:09:25 +0300

    Books UI

--- ClientApp/src/app/services/book.service.ts
+++ ClientApp/src/app/services/book.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  _baseURL : string = "api/Books";
+  constructor(private http : HttpClient) { }
+  
+  getAllBooks(){
+    return this.http.get<Book[]>(this._baseURL+"/GetBooks")
+  }
+
+
 }
