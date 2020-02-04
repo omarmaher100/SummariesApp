99f5189eb1dcda738701920e5aa72297dee1927c
Omar Maher <omarmaher100@gmail.com>
2020-02-04 12:23:20 +0300

    update book

--- ClientApp/src/app/components/update-book/update-book.component.ts
+++ ClientApp/src/app/components/update-book/update-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-book',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-book.component.css']
 })
 export class UpdateBookComponent implements OnInit {
+   book :Book;
+   updateBookForm : FormGroup;
 
-  constructor() { }
+  constructor(private service : BookService
+      ,private route : ActivatedRoute
+      ,private router : Router
+      ,private fb : FormBuilder) { }
 
   ngOnInit() {
+    this.service.GetBookById(this.route.snapshot.params.id).subscribe(data=>{
+      this.book = data;
+  
+
+    this.updateBookForm = this.fb.group({
+      id:[data.id],
+      title : [data.title ,Validators.required],
+      author : [data.author ,Validators.required],
+      description : [data.description ,Validators.compose([Validators.required,Validators.minLength(30)])],
+      rate:[data.rate],
+      dateStart:[this.formateDate(data.dateStart)],
+      dateRead:[this.formateDate(data.dateRead)]
+    });
+  })
+  }
+  formateDate(date : Date){
+    if(date)
+    return new Date(date).toISOString().substring(0,10);
   }
 
+
+  onSubmit(){
+    this.service.updateBook(this.updateBookForm.value).subscribe(data=>{
+      this.router.navigate(["/books"]);
+    })
+  }
 }
--- ClientApp/src/app/services/book.service.ts
+++ ClientApp/src/app/services/book.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ReturnStatement } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,9 @@ export class BookService {
 
   GetBookById(id : number){
     return this.http.get<Book>(this._baseURL+"/SingleBook/"+id)
+  }
+
+  updateBook(book :Book){
+    return this.http.put(this._baseURL+"/UpdateBook/"+book.id,book);
   }
 }
